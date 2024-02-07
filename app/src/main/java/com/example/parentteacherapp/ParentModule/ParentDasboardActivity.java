ab6a7f2a3cfed1e5def7de6e9a5bee0cc526b448
Shahroz <shahrozemuhammad198@gmail.com>
2024-02-07 15:50:29 +0500

    homefragment

--- app/src/main/java/com/example/parentteacherapp/ParentModule/ParentDasboardActivity.java
+++ app/src/main/java/com/example/parentteacherapp/ParentModule/ParentDasboardActivity.java
@@ -28,90 +28,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class ParentDasboardActivity extends AppCompatActivity {
-    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_dasboard);
-        LoadHomeFragmentData();
-        Navdrawer();
-    }
-    private void LoadHomeFragmentData(){
-        getSupportFragmentManager().
-                beginTransaction().
-                replace(R.id.framelayoutid,new UpdateParentProfileFragment())
-                .commit();
-    }
-    private void Navdrawer(){
-        NavigationView navigationView;
-        DrawerLayout drawerLayout;
-        Toolbar toolbar=findViewById(R.id.toolbarid);
-        setSupportActionBar(toolbar);
-        navigationView=findViewById(R.id.nav_viewid);
-        drawerLayout=findViewById(R.id.drawerlayoutid);
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
-        actionBarDrawerToggle.syncState();
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                if (item.getItemId() == R.id.updateuserpro) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new UpdateParentProfileFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.lisTofTeacher) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new ListOFTeacherParentFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.TeachersID) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new BookedTeacherParentFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.viewEventsmenuID) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new ViewEventParentFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.viewAbsentNotificationmenuID) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new ViewAbsentNotificationFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.chatwithTeachers) {
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.framelayoutid, new ChatwithTeacherFragment())
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                } else if (item.getItemId() == R.id.signout) {
-                    FirebaseAuth.getInstance().signOut();
-                    Toast.makeText(ParentDasboardActivity.this, "Signout", Toast.LENGTH_SHORT).show();
-
-                    Intent i = new Intent(ParentDasboardActivity.this, SignInActivity.class);
-                    startActivity(i);
-                }
-
-                return true;
-            }
-        });
+        
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
