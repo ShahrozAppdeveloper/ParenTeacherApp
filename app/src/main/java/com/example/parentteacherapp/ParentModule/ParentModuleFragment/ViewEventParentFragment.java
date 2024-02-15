8ab2e4448a14484d9d4aa41cfd9f249bc7094281
Shahroz <shahrozemuhammad198@gmail.com>
2024-02-15 19:16:43 +0500

    chatimplemented

--- app/src/main/java/com/example/parentteacherapp/ParentModule/ParentModuleFragment/ViewEventParentFragment.java
+++ app/src/main/java/com/example/parentteacherapp/ParentModule/ParentModuleFragment/ViewEventParentFragment.java
@@ -9,58 +9,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.parentteacherapp.R;
+import com.example.parentteacherapp.databinding.FragmentHomeBinding;
+import com.example.parentteacherapp.databinding.FragmentViewEventParentBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ViewEventParentFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class ViewEventParentFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+    private FragmentViewEventParentBinding binding;
     public ViewEventParentFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ViewEventParentFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ViewEventParentFragment newInstance(String param1, String param2) {
-        ViewEventParentFragment fragment = new ViewEventParentFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_event_parent, container, false);
+        binding = FragmentViewEventParentBinding.inflate(inflater,container,false);
+
+        return binding.getRoot();
     }
 }
